5e0c202185d76986f8e39822c802f20b82eba439
yungseok <mkoko8855@naver.com>
2023-04-21 18:22:39 +0900

    20230421

---
+++ db연동.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE members (
+    mem_id VARCHAR(20) PRIMARY KEY,
+    mem_pw VARCHAR2(20) NOT NULL,
+    mem_name VARCHAR2(20) NOT NULL,
+    mem_age NUMBER(2),
+    mem_regdate DATE DEFAULT sysdate
+);
